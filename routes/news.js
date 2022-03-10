5aa7575068465b9dd0883221270da2e9b4a17a5f
given-dragon <yijy001@gmail.com>
2022-03-10 22:33:15 +0900

    run python script

--- routes/news.js
+++ routes/news.js
@@ -6,6 +6,8 @@ const {getUid} = require('./middlewares');
 const sequelize = require('sequelize');
 const router = express.Router();
 
+const pythonShell = require('python-shell');
+
 // router.post('/', async (req, res, next) => {
 //     // const date = new Date().toString();
 //     const news = await News.create({
@@ -22,7 +24,7 @@ const router = express.Router();
 
 //뉴스의 기본정보(id, 제목, 등등)을 모두 보냄 -> 뉴스탭에 표시
 router.get('/', getUid, async (req, res, next) => {
-
+    //읽었던 뉴스 아이디 검색
     const readNewsId = 
         await User.findOne({where:{uid:req.uid}})
             .then(async (user) => {                
@@ -36,11 +38,10 @@ router.get('/', getUid, async (req, res, next) => {
                 console.error(error);
                 return next(error);
             });
-            
-    console.log(readNewsId);
 
     News.findAll({attributes:[
             'id', 'date', 'title', 'point',
+            //isRead 값 추가(읽었던 뉴스면 true, 아니면 false)
             [sequelize.literal(`CASE WHEN id IN (${readNewsId}) THEN ${true} ELSE ${false} END`), 'isRead']
         ]})
         .then((news) => {
@@ -54,14 +55,39 @@ router.get('/', getUid, async (req, res, next) => {
 
 //뉴스의 본문을 요약해서 반환
 router.get('/:newsId', async (req, res, next) => {
-    // const user = await User.findOne({where:{uid:req.uid}});
     const newsUrl = await News.findOne({
+        raw:true,
         where:{id:req.params.newsId},
         attributes:['url'],
     });
-    return res.json({message: 'return test news'});
+
+    //DB에 뉴스가 있는지 확인
+    if(newsUrl){
+        //set option for run python script
+        const options = {
+            mode:'text',
+            pythonPath:'',
+            pythonOptions:['-u'],
+            scriptPath:'./',
+            args:[newsUrl['url']]
+        };
+        //run python script
+        pythonShell.PythonShell.run('pythontest.py', options, function(error, result){
+            if(error){
+                console.error(error);
+                return next(error);
+            }
+            console.log(result);
+            return res.json({state:'success', summarized_text: result});
+        });
+    }else{
+        return res.status(430).json({state:'fail', message:`cant find news newsId:${req.params.newsId}`});
+    }
+    
+    
     //머신러닝 코드 돌리고 요약된 내용 리턴
 });
+
 //뉴스를 다 읽으면 점수 부여
 router.post('/:newsId', getUid, async (req, res, next) => {
     const user = await User.findOne({where:{uid:req.uid}});
